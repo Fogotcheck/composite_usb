2101d06055e0b8ffbbee47c41f382c1c33ce219c
JFogotcheck <fogotcheck@live.com>
2024-04-25 12:16:17 +0300

    CompositeUsb: add my descriptor

--- lib/CompositeUsb/ReportTypes/HidType.h
+++ lib/CompositeUsb/ReportTypes/HidType.h
@@ -1,4 +1,34 @@
 #ifndef __HidType_h__
 #define __HidType_h__
 
+
+typedef struct Stick
+{
+	uint16_t X;	 // Данные оси X первого стика (12 бит)
+	uint16_t Y;	 // Данные оси Y первого стика (12 бит)
+	uint16_t Z;	 // Данные оси X второго стика (12 бит)
+	uint16_t rX; // Данные оси Y второго стика (12 бит)
+	uint16_t rY; // Данные оси X третьего стика (12 бит)
+	uint16_t rZ; // Данные оси Y третьего стика (12 бит)
+
+} Stick_t;
+
+typedef struct Slider
+{
+	uint16_t L; // Данные оси X двухосевого стика (12 бит)
+	uint16_t R; // Данные оси Y двухосевого стика (12 бит)
+} Slider_t;
+
+#pragma pack(push, 1)
+typedef struct HIDReport
+{
+
+	uint32_t Buttons;
+	Stick_t Stick;
+	Slider_t Slider;
+	uint8_t wake;
+
+} HIDReport_t;
+#pragma pack(pop)
+
 #endif //__HidType_h__
--- lib/CompositeUsb/src/CompositeUsb.c
+++ lib/CompositeUsb/src/CompositeUsb.c
@@ -13,6 +13,8 @@ void UsbHidThread(void *arg);
 void USB_HP_IRQHandler(void);
 void USB_LP_IRQHandler(void);
 
+void BtnTest(uint32_t *btn);
+
 int CompositeUsbInit(void)
 {
   BaseType_t ret = xTaskCreate(UsbMainThread, "UsbMainTask", 512, NULL,
@@ -23,11 +25,14 @@ int CompositeUsbInit(void)
 
 void UsbMainThread(__attribute__((unused)) void *arg)
 {
+  CdcReport_t Buf = {0};
+  Buf.DType = CDC_RX;
+  BaseType_t ret = 0;
 
   HAL_PWREx_EnableVddUSB();
   __HAL_RCC_USB_CLK_ENABLE();
   printf("tusb::\t%hhu\r\n", tud_init(BOARD_TUD_RHPORT));
-  BaseType_t ret = 0;
+
   ret = xTaskCreate(UsbCdcThread, "UsbCdcTask", 512, NULL,
                     2, &UsbCdcHandle);
   if (ret != pdPASS)
@@ -35,18 +40,26 @@ void UsbMainThread(__attribute__((unused)) void *arg)
     printf("Cdc thr err\r\n");
   }
 
-  // ret = xTaskCreate(UsbHidThread, "UsbHidTask", 512, NULL,
-  //                   2, &UsbHidHandle);
-  // if (ret != pdPASS)
-  // {
-  //   printf("Hid thr err\r\n");
-  // }
+  ret = xTaskCreate(UsbHidThread, "UsbHidTask", 512, NULL,
+                    2, &UsbHidHandle);
+  if (ret != pdPASS)
+  {
+    printf("Hid thr err\r\n");
+  }
   printf("Composite usb::\tinit\r\n");
   while (1)
   {
     tud_task(); // device task
+    if (tud_cdc_n_available(0))
+    {
+      Buf.Size = tud_cdc_n_read(0, Buf.Data, sizeof(Buf.Data));
+      if (xQueueSend(UsbCdcQueue, &Buf, 5) != pdPASS)
+      {
+        printf("Cdc RX err\r\n");
+      }
+    }
 
-    vTaskDelay(3);
+    vTaskDelay(5);
   }
 }
 
@@ -64,21 +77,65 @@ void UsbCdcThread(__attribute__((unused)) void *arg)
       tud_cdc_n_write_flush(0);
       break;
     case CDC_RX:
-      /* code */
+    {
+      char tmp[] = "reciev::\t";
+      tud_cdc_n_write(0, tmp, sizeof(tmp));
+      tud_cdc_n_write(0, Buf.Data, Buf.Size);
+      tud_cdc_n_write_flush(0);
       break;
-
+    }
     default:
       break;
     }
-
-    vTaskDelay(1000);
   }
 }
+
 void UsbHidThread(__attribute__((unused)) void *arg)
 {
+  UsbHidQueue = xQueueCreate(USB_CDC_QUEUE_LEN, sizeof(CdcReport_t));
+  HIDReport_t report = {0};
   while (1)
   {
-    /* code */
+
+    if (!tud_hid_ready())
+    {
+      continue;
+    }
+    BtnTest(&report.Buttons);
+    tud_hid_report(0, &report, sizeof(report));
+    vTaskDelay(10);
+  }
+}
+
+void BtnTest(uint32_t *btn)
+{
+  static uint8_t state = 0;
+  static uint32_t mask = 1;
+  switch (state)
+  {
+  case 0:
+    if (*btn >= 0xfffff)
+    {
+      mask = 1;
+      state++;
+      break;
+    }
+    *btn |= mask;
+    mask <<= 1;
+    break;
+  case 1:
+    if (*btn == 0)
+    {
+      mask = 1;
+      state++;
+      break;
+    }
+    *btn &= ~mask;
+    mask <<= 1;
+    break;
+  default:
+    state = 0;
+    break;
   }
 }
 //--------------------------------------------------------------------+
