aab6e9bc79978320c67aa5dd8bdc4c22f864002b
JFogotcheck <fogotcheck@live.com>
2024-04-26 14:24:46 +0300

    Debug: add state printf info

--- lib/Debug/head/Debug_MSG.h
+++ lib/Debug/head/Debug_MSG.h
@@ -2,14 +2,16 @@
 #define __Debug_MSG_h__
 #include <stdio.h>
 
+/* DEBUG INFO LEVEL */
 #if defined(DEBUG_INFO)
 #define D_INIT_INFO printf("%s::\tinit end\r\n", __FUNCTION__)
-
+#define D_STATE_INFO printf("printf::\tDisable\r\n")
 #else
 #define D_INIT_INFO
-
+#define D_STATE_INFO
 #endif
 
+/* DEBUG ERRORS LEVEL */
 #if defined(DEBUG_ERR)
 #define D_ERR_MSG_L0 printf("%s::\tcrit err\tline::\t%lu\r\n", __FUNCTION__, __LINE__)
 #define D_ERR_MSG_L1 printf("%s::\tl1 err\tline::\t%lu\r\n", __FUNCTION__, __LINE__)
--- lib/Debug/src/Debug_Print.c
+++ lib/Debug/src/Debug_Print.c
@@ -24,20 +24,29 @@ PUTCHAR_PROTOTYPE
 */
 void DebugPrintSwitch(uint8_t state)
 {
+	if (state == DEBUG_PRINT_DISABLE)
+	{
+		DebugPrintEn = DEBUG_PRINT_ENABLE;
+		D_STATE_INFO;
+	}
 	DebugPrintEn = state;
 }
 
 int DebugTerminal_send(uint8_t data)
 {
-	if (DebugPrintEn != DEBUG_PRINT_ENABLE) {
+	if (DebugPrintEn != DEBUG_PRINT_ENABLE)
+	{
 		return 0;
 	}
 
 	HAL_StatusTypeDef ret;
 	ret = HAL_UART_Transmit(DebugUart, &data, 1, 5);
-	if (ret == HAL_OK) {
+	if (ret == HAL_OK)
+	{
 		return 0;
-	} else {
+	}
+	else
+	{
 		return 1;
 	}
 }
